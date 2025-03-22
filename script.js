572d2d4a619731495eeec0b3cca42e6e39a222c7
Khushboo <dalalkhushboo410@gmail.com>
2025-03-22 19:37:01 +0530

    msg

--- script.js
+++ script.js
@@ -1,117 +1,107 @@
+
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
 });
-
-
+// local reviews data
 const reviews = [
-    {
-      id: 1,
-      name: 'Vince Taylor',
-      job: 'McMOHAN TRANSPORTATION',
-      img: 'me.jpg',
-      text: "INTERHAUL LOGISTICS is the best!! I just focus on driving and arriving the destination at time, they take care of the rest",
+  {
+    id: 1,
+    name: 'Khushboo Dalal',
+    job: 'web developer',
+    img: 'WhatsApp Image 2025-03-15 at 11.38.29_a62573ee.jpg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'Chiku',
+    job: 'web designer',
+    img: 'WhatsApp Image 2025-03-15 at 11.38.29_edb13bce.jpg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Chiki',
+    job: 'intern',
+    img: 'WhatsApp Image 2025-03-15 at 11.38.29_ae8086df.jpg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'Shayar',
+    job: 'the boss',
+    img: 'WhatsApp Image 2025-03-15 at 11.38.29_7cabd8ff.jpg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+      id: 5,
+      name: 'Me',
+      job: 'the boss',
+      img: 'WhatsApp Image 2025-03-15 at 11.29.08_db20136c.jpg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
     },
-    {
-      id: 2,
-      name: 'Syed',
-      job: 'ABBAS TRANSPORTATION',
-      img: 'me1.jpg',
-      text: 'My previous dispatchers will never show me the RATE CONFIRMATION, but with INTERHAUL LOGISTICS  every information I need is a click away.',
-    },
-    {
-      id: 3,
-      name: 'Chiki',
-      job: 'XYZ CARRIERS',
-      img: 'me2.jpg',
-      text: 'INTERHAUL LOGISTICS provides more than just good loads, they know where I want to go, what my priorities are, and almost never fail to send me home when I need to go home',
-    },
-    {
-      id: 4,
-      name: 'Shayar',
-      job: 'OPTIMUS PRIME LLC',
-      img: 'me3.jpg',
-      text: 'I never have to search anything on the phone while driving, whether it’s a restaurant, or a parking location, or a warehouse, my dispatcher will just send a link that I need to click ',
-    },
-    {
-        id: 5,
-        name: 'Me',
-        job: 'STS PAK LLC',
-        img: 'me4.jpg',
-        text: 'Since I started working with INTERHAUL LOGISTICS, it feels like I have more hours in a day. With their Hours-Of-Service assistance I get more out of my ELD now ',
-      },
-  ];
-  // select items
-  const img = document.getElementById('person-img');
-  const author = document.getElementById('author');
-  const job = document.getElementById('job');
-  const info = document.getElementById('info');
-  
-  const prevBtn = document.querySelector('.prev-btn');
-  const nextBtn = document.querySelector('.next-btn');
-  const randomBtn = document.querySelector('.random-btn');
-  
-  // set starting item
-  let currentItem = 0;
-  
-  // load initial item
-  window.addEventListener('DOMContentLoaded', function () {
-    const item = reviews[currentItem];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
-  });
-  
-  // show person based on item
-  function showPerson(person) {
-    const item = reviews[person];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
-  }
-  // show next person
-  nextBtn.addEventListener('click', function () {
-    currentItem++;
-    if (currentItem > reviews.length - 1) {
-      currentItem = 0;
-    }
-    showPerson(currentItem);
-  });
-  // show prev person
-  prevBtn.addEventListener('click', function () {
-    currentItem--;
-    if (currentItem < 0) {
-      currentItem = reviews.length - 1;
-    }
-    showPerson(currentItem);
-  });
-  // show random person
-  randomBtn.addEventListener('click', function () {
-    console.log('hello');
-  
-    currentItem = Math.floor(Math.random() * reviews.length);
-    showPerson(currentItem);
-  });
+];
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
-  const questions = document.querySelectorAll(".question");
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
-questions.forEach(function (question) {
-  const btn = question.querySelector(".question-btn");
-  // console.log(btn);
+// set starting item
+let currentItem = 0;
 
-  btn.addEventListener("click", function () {
-    // console.log(question);
-
-    questions.forEach(function (item) {
-      if (item !== question) {
-        item.classList.remove("show-text");
-      }
-    });
-
-    question.classList.toggle("show-text");
-  });
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
 });
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+// show random person
+randomBtn.addEventListener('click', function () {
+  console.log('hello');
+
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
+
+const accordion = document.getElementsByClassName('content-container');
+
+for(i = 0; i < accordion.length; i++){
+
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+    })
+};
