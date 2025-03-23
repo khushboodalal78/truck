45947ee5e855c8a1e78b98d2d2c961458fb8a156
Khushboo <dalalkhushboo410@gmail.com>
2025-03-23 13:23:35 +0530

    msg

--- script.js
+++ script.js
@@ -96,3 +96,11 @@ randomBtn.addEventListener('click', function () {
   currentItem = Math.floor(Math.random() * reviews.length);
   showPerson(currentItem);
 });
+const accordion = document.getElementsByClassName('content-container');
+
+for(i = 0; i < accordion.length; i++){
+
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+    })
+};
